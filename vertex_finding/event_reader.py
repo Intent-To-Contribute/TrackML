d218dd7f16699f88a5c7f1c90633fef4d80f93ee
Lawrence Hook <lawrencehook@gmail.com>
2018-05-01 19:50:59 -0400

    rearrange files

--- vertex_finding/event_reader.py
+++ vertex_finding/event_reader.py
@@ -1,5 +1,21 @@
 from trackml.dataset import load_event
+import pdb
 #hits, cells, particles, truth = load_event('path/to/event000000123')
 hits, cells, particles, truth = load_event('../..//Data/train_100_events/event000001052')
-print(hits)
+#print(hits)
+#print(truth["particle_id"])
+pdb.set_trace()
+print(len(truth["particle_id"].unique))
 
+
+# print(hits.iloc[0])
+
+"""
+for i in range(len(hits)):
+  # print(hits.iloc[i])
+  
+  for val in hits.iloc[i]:
+    print(val),
+
+  print("\n")
+"""
