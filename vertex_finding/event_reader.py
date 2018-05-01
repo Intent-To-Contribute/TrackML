ff317ffe422c966a3fae77b4096ddbf5e4faa4fd
Ryan Bouabid <ryan-bouabid@live.com>
2018-05-02 00:09:45 +0200

    big

---
+++ vertex_finding/event_reader.py
@@ -0,0 +1,5 @@
+from trackml.dataset import load_event
+#hits, cells, particles, truth = load_event('path/to/event000000123')
+hits, cells, particles, truth = load_event('../..//Data/train_100_events/event000001052')
+print(hits)
+
