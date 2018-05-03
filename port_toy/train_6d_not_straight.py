5928f7f4213dc46e2b6c581e2de62a8d42b48b15
Lawrence Hook <lawrencehook@gmail.com>
2018-05-03 11:40:42 -0400

    remove all 0 input sequences, start training

--- port_toy/train_6d_not_straight.py
+++ port_toy/train_6d_not_straight.py
@@ -5,14 +5,15 @@ import pandas as pd
 import numpy as np
 from trackml.dataset import load_event
 
+
+### load data ###
 hits, cells, particles, truth = load_event('../..//Data/train_100_events/event000001052')
 
-sorted_truth = truth.sort_values("particle_id")
-#print(sorted_truth)
 
+### group hits into their true tracks ###
+sorted_truth = truth.sort_values("particle_id")
 true_tracks = []
 track = []
-
 current_pid = -1
 if not os.path.exists("all_tracks.npy"):
     for i in range(len(sorted_truth)):
@@ -29,8 +30,10 @@ if not os.path.exists("all_tracks.npy"):
 else:
     true_tracks = np.load("all_tracks.npy")
 
-print("finished initializeing true_tracks")
+print("finished initializing true_tracks")
 
+
+### Format input sequences X and output Y ###
 max_len = 0
 X = []
 Y = []
@@ -43,20 +46,23 @@ for track in true_tracks:
 # --- making input vectors ---
 for track in true_tracks:
     curr_idx = 0
-    for i in range(max_len-1):
+    for i in range(1, max_len-1):
         x_hit = np.zeros((max_len, 6))
         if i < len(track)-1:
             for z in range(i):
-                x_hit[max_len-i+z] = track[z][2:8] 
-            Y.append(track[i+1])
+                x_hit[max_len-i+z] = track[z][2:8]
             X.append(x_hit)
+            # Y.append(track[i+1][2:8])
+            Y.append(track[i+1][2:5])
         
 
 X = np.asarray(X)
 Y = np.asarray(Y)
 print(X.shape, Y.shape)
 
-"""
+print(X[0])
+print(Y[0])
+
 
 import keras
 from keras.models import Sequential
@@ -64,30 +70,37 @@ from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
 from keras.models import load_model
 
-in_out_neurons = 2
-hidden_neurons = 300
+### train model ###
+seq_len = max_len
+in_neurons = 6
+out_neurons = 3
+hidden_neurons = 500
 
 model = Sequential()
+# model.add(LSTM(hidden_neurons, return_sequences=False,
+				# input_shape=(None, in_neurons)))
 model.add(LSTM(hidden_neurons, return_sequences=False,
-				input_shape=(None, in_out_neurons)))
-model.add(Dense(in_out_neurons, input_dim=hidden_neurons))
+                input_shape=(seq_len, in_neurons)))
+model.add(Dense(out_neurons, input_dim=hidden_neurons))
 model.add(Activation("linear"))
 model.compile(loss="mean_squared_error", optimizer="rmsprop")
 
 print(model.summary())
 
 
-# model.fit(x, y, batch_size=20, epochs=10, validation_split=0.05)
-# model.save("model.keras")
-model = load_model("model.keras")
+model.fit(X, Y, batch_size=2000, epochs=1, validation_split=0.05)
+model.save("model.keras")
+# model = load_model("model.keras")
 
 
-predicted = model.predict(x)
+predicted = model.predict(X)
 print(predicted.shape)
 
-import matplotlib.pylab as plt
-plt.scatter(predicted[:,0], predicted[:,1], color="red", s=1)
-plt.scatter(y[:,0], y[:,1], color="blue", s=1)
-plt.legend(["Predicted", "Actual"])
-plt.show()
-"""
+
+### viz ###
+# import matplotlib.pylab as plt
+# plt.scatter(predicted[:,0], predicted[:,1], color="red", s=1)
+# plt.scatter(y[:,0], y[:,1], color="blue", s=1)
+# plt.legend(["Predicted", "Actual"])
+# plt.show()
+
